42273518d87fdaf80489d79185817c53dde5e12e
Ryan Boudwin <rboudwin@student.hive.fi>
2024-02-05 10:20:31 +0200

    now parsing paths appropriately into an array of strings we can use later

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rboudwin <rboudwin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 17:17:53 by rboudwin          #+#    #+#             */
-/*   Updated: 2024/01/31 14:11:08 by rboudwin         ###   ########.fr       */
+/*   Updated: 2024/02/05 10:19:47 by rboudwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,31 @@
 
 char	**parse_paths(char **env)
 {
-	int	i;
+	int		i;
+	char	**paths;
 
 	i = 0;
-	//ft_printf("'%s'", env[0]);
 	while (env[i] != NULL)
-		ft_printf("%s\n", env[i++]);
-	return (env);
+	{
+		if (ft_strnstr(env[i], "PATH=", 5))
+			paths = ft_split(env[i] + 5, ':');
+		i++;
+	}
+	return (paths);
 }
 
 int	main(int argc, char **argv, char **env)
 {
-//	if (argc != 5)
-//		exit(EXIT_FAILURE);
-	parse_paths(env);
+	char	**paths;
+	int		i;
+
+	i = 0;
+	if (argc != 5)
+	{
+		ft_putstr_fd("ERROR: exactly four arguments required\n", 2);
+		exit(EXIT_FAILURE);
+	}
+	paths = parse_paths(env);
+	while (paths[i])
+		ft_printf("%s\n", paths[i++]);
 }
