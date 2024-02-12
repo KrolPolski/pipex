a354ef38020eb8fa619c1c211f35df318e077c52
Ryan Boudwin <rboudwin@student.hive.fi>
2024-02-12 13:09:55 +0200

    removed duplicate logic

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rboudwin <rboudwin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 17:17:53 by rboudwin          #+#    #+#             */
-/*   Updated: 2024/02/12 13:03:15 by rboudwin         ###   ########.fr       */
+/*   Updated: 2024/02/12 13:09:20 by rboudwin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -116,12 +116,8 @@ int	validate_arguments(t_pipex *p)
 	return (check_output_file(p));
 }
 
-int	main(int argc, char **argv, char **env)
+void	check_arg_count(int argc, char **argv)
 {
-	int		i;
-	t_pipex	p;
-
-	i = 0;
 	if (argc != 5)
 	{
 		ft_putstr_fd("ERROR: exactly four arguments required\n", 2);
@@ -129,15 +125,29 @@ int	main(int argc, char **argv, char **env)
 	}
 	if (!argv[1][0] || !argv[2][0] || !argv[3][0] || !argv[4][0])
 	{
+		//consider that this error doesn't match shell behavior
 		ft_putstr_fd("ERROR: empty string detected\n", 2);
 		exit(EXIT_FAILURE);
 	}
+}
+
+int	main(int argc, char **argv, char **env)
+{
+	int		i;
+	t_pipex	p;
+
+	i = 0;
+	check_arg_count(argc, argv);
 	p.argc = argc;
 	p.argv = argv;
 	p.env = env;
 	p.cmd1 = ft_split(argv[2], ' ');
 	p.cmd2 = ft_split(argv[3], ' ');
-	//need to detect split failures
+	if (!p.cmd1 || !p.cmd2)
+	{
+		//consider what to free here and if we need an error msg
+		exit(EXIT_FAILURE);
+	}
 	p.paths = parse_paths(env);
 	p.output = 1;
 	p.cmd_with_path = malloc(sizeof(char *) * (argc - 2));
@@ -148,15 +158,7 @@ int	main(int argc, char **argv, char **env)
 		free_2d(p.cmd2);
 		exit(EXIT_FAILURE);
 	}
-	if (validate_arguments(&p) == -1)
-	{
-		free_2d(p.paths);
-		free_2d(p.cmd1);
-		free_2d(p.cmd2);
-		free_2d(p.cmd_with_path);
-		exit(EXIT_FAILURE);
-	}
-	if (pipex(&p) < 0)
+	if (validate_arguments(&p) == -1 || pipex(&p) < 0)
 	{
 		free_2d(p.paths);
 		free_2d(p.cmd1);
